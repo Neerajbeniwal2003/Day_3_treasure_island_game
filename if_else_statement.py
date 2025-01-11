48ab3396d3234398716b4d534257eed4fc42a2e7
[ Ne_eraj009 ] <neerajbeniwal2015@gmail.com>
2025-01-11 18:00:40 +0530

    Update if_else_statement.py

---
+++ if_else_statement.py
@@ -0,0 +1,15 @@
+print("welcome to roler coster")
+height=int(input("what is your height:"))
+if height > 120:
+    print("you can enjoye the ride")
+else:
+    print("you are not eligible")
+
+
+#COMPARISSION OPERATOR
+#<  less than
+#>  greater then 
+#<= less then equal to
+#>= greater then equal to
+#== equal to
+#!= not equal to
